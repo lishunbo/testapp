9f847de362f36fb8536702581c0a63a3f0baf12f
lishunbo <lishunbo@shengwang.cn>
2024-03-06 16:49:37 +0800

    init commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+
+import {version} from './version'
+
+console.log("abc", version)
+
+export default function HelloDemo() {
+  console.log('helloDemo')
+  console.log('version:', version)
+}
---
+++ src/version.ts
@@ -0,0 +1,5 @@
+function getVersion() {
+  return '1.0.0';
+}
+
+export const version = getVersion();
